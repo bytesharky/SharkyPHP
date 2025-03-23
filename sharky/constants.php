007cda4baccac29223fcd310469a897820692e41
bytesharky <fish@doffish.com>
2025-03-23 11:38:00 +0800

    composer版本

--- sharky/constants.php
+++ sharky/constants.php
@@ -15,8 +15,8 @@ define('COPYRIGHT', getCopyright(2024,'Forgot Fish'));
 // 定义框架目录常量
 define('SHARKY_ROOT', realpath(__DIR__ . '/../sharky'));
 
- // 定义应用程序根目录
- define('APP_ROOT', realpath(__DIR__ . '/../app'));
+// 定义应用程序根目录
+define('APP_ROOT', realpath(__DIR__ . '/../app'));
 
 // 站点目录
 define('SITE_ROOT', getSitePath());
